ccc06203e09356e30a9eb28b67039147dc13c58d
OpenPadavan <OpenPadavan@proton.me>
2026-08-11 21:47:20 +0100

    fix: always heap-allocate msg_ctxt_id in ndisc6 gettext.h to avoid returning stack memory

--- trunk/user/ndisc6/ndisc6-1.0.8/include/gettext.h
+++ trunk/user/ndisc6/ndisc6-1.0.8/include/gettext.h
@@ -223,27 +223,16 @@ dcpgettext_expr (const char *domain,
   size_t msgctxt_len = strlen (msgctxt) + 1;
   size_t msgid_len = strlen (msgid) + 1;
   const char *translation;
-#if _LIBGETTEXT_HAVE_VARIABLE_SIZE_ARRAYS
-  char msg_ctxt_id[msgctxt_len + msgid_len];
-#else
-  char buf[1024];
-  char *msg_ctxt_id =
-    (msgctxt_len + msgid_len <= sizeof (buf)
-     ? buf
-     : (char *) malloc (msgctxt_len + msgid_len));
+  char *msg_ctxt_id = (char *) malloc (msgctxt_len + msgid_len);
   if (msg_ctxt_id != NULL)
-#endif
     {
       int found_translation;
       memcpy (msg_ctxt_id, msgctxt, msgctxt_len - 1);
       msg_ctxt_id[msgctxt_len - 1] = '\004';
       memcpy (msg_ctxt_id + msgctxt_len, msgid, msgid_len);
       translation = dcgettext (domain, msg_ctxt_id, category);
-      found_translation = (translation != msg_ctxt_id);
-#if !_LIBGETTEXT_HAVE_VARIABLE_SIZE_ARRAYS
-      if (msg_ctxt_id != buf)
-        free (msg_ctxt_id);
-#endif
+      found_translation = (translation != msg_ctxt_id && translation != msgid);
+      free (msg_ctxt_id);
       if (found_translation)
         return translation;
     }
@@ -271,16 +260,8 @@ dcnpgettext_expr (const char *domain,
   size_t msgctxt_len = strlen (msgctxt) + 1;
   size_t msgid_len = strlen (msgid) + 1;
   const char *translation;
-#if _LIBGETTEXT_HAVE_VARIABLE_SIZE_ARRAYS
-  char msg_ctxt_id[msgctxt_len + msgid_len];
-#else
-  char buf[1024];
-  char *msg_ctxt_id =
-    (msgctxt_len + msgid_len <= sizeof (buf)
-     ? buf
-     : (char *) malloc (msgctxt_len + msgid_len));
+  char *msg_ctxt_id = (char *) malloc (msgctxt_len + msgid_len);
   if (msg_ctxt_id != NULL)
-#endif
     {
       int found_translation;
       memcpy (msg_ctxt_id, msgctxt, msgctxt_len - 1);
@@ -288,10 +269,7 @@ dcnpgettext_expr (const char *domain,
       memcpy (msg_ctxt_id + msgctxt_len, msgid, msgid_len);
       translation = dcngettext (domain, msg_ctxt_id, msgid_plural, n, category);
       found_translation = !(translation == msg_ctxt_id || translation == msgid_plural);
-#if !_LIBGETTEXT_HAVE_VARIABLE_SIZE_ARRAYS
-      if (msg_ctxt_id != buf)
-        free (msg_ctxt_id);
-#endif
+      free (msg_ctxt_id);
       if (found_translation)
         return translation;
     }
